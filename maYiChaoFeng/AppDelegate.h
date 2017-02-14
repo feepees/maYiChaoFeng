d3fe34e4c896c9c9a42824f2571107b200e9c3da
Android <cehua@cehuadeimac.local>
2017-02-14 10:47:14 +0800

    Initial Commit

---
+++ maYiChaoFeng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  maYiChaoFeng
+//
+//  Created by Android on 2017/2/14.
+//  Copyright © 2017年 cc.youdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ maYiChaoFeng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  maYiChaoFeng
+//
+//  Created by Android on 2017/2/14.
+//  Copyright © 2017年 cc.youdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
